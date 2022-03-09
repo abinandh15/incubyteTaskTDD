410a8e747659745e7d470f1d14d2969364f71c9c
Abinandh <abinandh15@gmail.com>
2022-03-09 12:00:53 +0530

    test: accept unknown amount of numbers

--- tests/index.test.js
+++ tests/index.test.js
@@ -13,3 +13,16 @@ describe('Add Task 1', ()=>{
         expect(Add("1,2")).toBe(3)
     })
 })
+
+describe('Add task 2 - unknown amount of numbers', ()=>{
+    it('should handle unknown amount of numbers',()=>{
+        expect(Add("1,5,4,3,8,10,500,650")).toBe(1181);
+        expect(Add("2,5,6,4,8,1,2")).toBe(28);
+        let input = "1"
+        for(let i=2; i<=100; i++){
+            input = input + ',' + i;
+        }
+        expect(Add(input))
+        .toBe(5050)
+    })
+})
