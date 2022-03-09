8440fd0a33404dcd97257e93cf95c21376863252
Abinandh <abinandh15@gmail.com>
2022-03-09 11:51:38 +0530

    feat: all test cases passed, Task 1.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function Add(str) {
-    if(str === ""){
+    if (str === "") {
         return 0
+    } else if (str.length === 1) {
+        return +str
     }
+
+
+    const numberArray = str.split(',');
+    
+    // solution using reduce method
+    // return numberArray.reduce((prevVal, currentVal) => (+prevVal) + (+currentVal), 0)
+
+    let sum = 0;
+    numberArray.forEach(number=>{
+        sum += +number;
+    })
+    return sum;
 }
 
 module.exports = Add;
