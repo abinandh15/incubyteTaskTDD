64781d8a2494f1609cd99ab085db1f0fc0a23bc3
Abinandh <abinandh15@gmail.com>
2022-03-09 11:42:17 +0530

    tests: written all test cases for task 1

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,8 +2,14 @@ const { test, expect, describe, it } = require("@jest/globals");
 const Add = require("..");
 
 
-describe('add', ()=>{
+describe('Add Task 1', ()=>{
     it('should return 0 for an empty string', ()=>{
         expect(Add('')).toBe(0)
+    })
+    it('should return 1 for input "1"', ()=>{
+        expect(Add("1")).toBe(1)
+    })
+    it('should return 3 for input "1,2"', ()=>{
+        expect(Add("1,2")).toBe(3)
     })
 })
